fb0cb8edd12572414a34ee21b121534e6dcd2490
lariszu <breazdau.larisa@gmail.com>
2022-05-27 16:06:48 +0300

    26 mai

---
+++ M01S04/ex01/app.js
@@ -0,0 +1,21 @@
+var value = window.prompt('Introdu un numar');
+var message = '';
+var messageParagraph = document.getElementById('message');
+var number = window.prompt('Cu ce numar sa compar?');
+
+console.log('Valoarea este: ' + value);
+
+value = Number(value);
+
+if (isNaN(value) === true) {
+  message = 'Nu ai introdus un numar';
+} else if (value === number) {
+  message = `Numarul este ${number}`;
+} else if (value > number) {
+  message = `Numarul ${value} este mai mare decat ${number}`;
+} else {
+  message = `Numarul ${value} este mai mic decat ${number}`;
+}
+
+console.log(message);
+messageParagraph.innerText = message;
